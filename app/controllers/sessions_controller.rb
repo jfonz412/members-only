bd6a438c8d683cb593f908adbc1d3d13a8f03115
jfonz412 <jfonzone@outlook.com>
2017-06-08 17:50:28 -0400

    user authenticated and returned to home on login success

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	@user = User.find_by(email: params[:session][:email])
+
+  	if @user && @user.authenticate(params[:session][:password])
+  		#log in user
+  		redirect_to root_url
+  	else
+  		flash[:warning] = "Incorrect email or password"
+  		render 'new'
+  	end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get    '/signup',    to: 'users#new'
   post   '/signup',    to: 'users#create'
   get    '/login', 	   to: 'sessions#new'
+  post   '/login',	   to: 'sessions#create'
   resources :users
 end
