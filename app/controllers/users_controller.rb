d749e88b7ed61003e0649776ce75e794d47b743f
jfonz412 <jfonzone@outlook.com>
2017-06-08 12:23:35 -0400

    fix styling and flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "Account created!"
+  		redirect_to @user
+  	else 
+  		render 'new'
+  	end
   end
+
+  def show
+  end
+
+  private
+  	def user_params
+  		# :user is the params[:user] nested hash the form creates when submited
+  		params.require(:user).permit(:name, :email, :password,
+  																 :password_confirmation)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root   'users#show'
   get    '/signup',    to: 'users#new'
   post   '/signup',    to: 'users#create'
   resources :users
