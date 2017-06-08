a0d583bced1ea9f4ed4602c27f099b142ffb5775
jfonz412 <jfonzone@outlook.com>
2017-06-08 12:02:48 -0400

    signup form + all sample_app css

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+	validates :name, presence: true, length: { minimum: 5, maximum: 25 }
+	validates :email, presence: true
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 6 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@user = User.new(name: "Foobar", email: "foo@bar.com", password: "foobar",
+										 													password_confirmation: "foobar")
+	end
+
+	test "should be valid" do
+		assert @user.valid?
+	end
+
+	test "name should be present" do
+		@user.name = "		"
+		assert_not @user.valid?
+	end
+
+	test "email should be present" do
+		@user.email = "		"
+		assert_not @user.valid?
+	end
+
+	test "name should not be too long" do
+		@user.name = "a" * 51
+		assert_not @user.valid?
+	end
+
+  test "password should be present (nonblank)" do
+  	@user.password = @user.password_confirmation = "	" * 6
+  	assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+  	@user.password = @user.password_confirmation = "a" * 5
+  	assert_not(@user.valid?)
+  end
 end
