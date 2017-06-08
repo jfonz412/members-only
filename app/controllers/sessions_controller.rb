00cf2dc87ad6ecd0c0e434f66b86a4b0d21a24a0
jfonz412 <jfonzone@outlook.com>
2017-06-08 18:14:03 -0400

    login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	@user = User.find_by(email: params[:session][:email])
 
   	if @user && @user.authenticate(params[:session][:password])
-  		#log in user
+  		log_in(@user)
   		redirect_to root_url
   	else
   		flash.now[:warning] = "Incorrect email or password"
