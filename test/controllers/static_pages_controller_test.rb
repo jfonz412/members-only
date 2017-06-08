f4a77c830f983ed53d10e0b0dc42a7e174bf4920
jfonz412 <jfonzone@outlook.com>
2017-06-08 16:18:17 -0400

    super basic site layout tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+
+	def setup
+		@base_title = "Members Only!"
+	end
+	
+  test "should get root" do
+    get root_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
 end
