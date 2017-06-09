dbad3c68f915f13ce0e468ebcd9fe7fe69c3fbf5
jfonz412 <jfonzone@outlook.com>
2017-06-09 17:22:48 -0400

    post validations

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,11 +4,12 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get newpost_url
     assert_response :success
+    assert_select "title", "New Post | Members Only!"
   end
 
   test "should get index" do
     get posts_url
     assert_response :success
+    assert_select "title", "All Posts | Members Only!"
   end
-
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@post = Post.new(title: "Test Post", body: "test"*10)
+	end
+
+	test "should be valid" do
+		assert @post.valid?
+	end
+
+	test "no title" do
+		@post.title = ""
+		assert_not @post.valid?
+	end
+
+	test "no body" do
+		@post.body = ""
+		assert_not @post.valid?
+	end
+
+	test "title too long" do
+		@post.title = "x" * 50
+		assert_not @post.valid?
+	end
+
+	test "body too long" do
+		@post.title = "x" * 600
+		assert_not @post.valid?
+	end
 end
